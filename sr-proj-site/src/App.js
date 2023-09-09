3b33b2c0139aa476bb4e7162631855059c14902d
jmw3638 <jmw3638@rit.edu>
2023-09-09 17:50:11 -0400

    Initial commit, bare-bones home page

---
+++ sr-proj-site/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <p>5G-PAWS</p>
+            <ul>
+                <li>Nick Grosskopf</li>
+                <li>Sasha Persaud</li>
+                <li>Patrick Sorensen</li>
+                <li>Jake Waclawski</li>
+                <li>Sakai Alexander</li>
+            </ul>
+        </div>
+    );
+}
+
+export default App;
