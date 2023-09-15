e058c1c2b6d3bca2c39203245b68979ab9aa4b88
Jacob Waclawski <54681308+jmw3638@users.noreply.github.com>
2023-09-15 11:52:21 -0400

    Add simple router for page navigation

---
+++ sr-proj-site/src/Home.js
@@ -0,0 +1,16 @@
+function Home() {
+    return (
+        <div className="home">
+            <p>5G-PAWS</p>
+            <ul>
+                <li>Nick Grosskopf</li>
+                <li>Sasha Persaud</li>
+                <li>Patrick Sorensen</li>
+                <li>Jake Waclawski</li>
+                <li>Sakai Alexander</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Home;
